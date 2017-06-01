50576b04f27d2b4eaadd31e3468f2ab9e9a42b7f
David Jennings <djennings@esri.com>
2017-06-01 13:54:45 -0500

    Added project

---
+++ mavenJavaScalalearning/src/main/java/org/jennings/java/learning/Main.java
@@ -0,0 +1,11 @@
+package org.jennings.java.learning;
+
+/**
+ * Created by david on 6/1/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ mavenJavaScalalearning/src/main/scala/org/jennings/scala/learning/Main.scala
@@ -0,0 +1,8 @@
+package org.jennings.scala.learning
+
+/**
+  * Created by david on 6/1/17.
+  */
+object Main extends App {
+  println("Hello Scala")
+}
